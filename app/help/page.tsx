b6549034ecb50efb46ac0b02478eadfddaad5c35
Jora <91777313+Jora001@users.noreply.github.com>
2024-04-30 11:16:24 +0400

    creating-help-page

---
+++ app/help/page.tsx
@@ -0,0 +1,13 @@
+import { PageTitle } from "@/components";
+import HelpForm from "@/components/molecules/helpForm";
+import React from "react";
+
+function Help() {
+  return (
+    <>
+      <HelpForm />
+    </>
+  );
+}
+
+export default Help;
