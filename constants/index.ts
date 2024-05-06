be655415d08e6ad735e34e6b341c5d44498dffda
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2024-05-06 10:37:58 +0400

    develop-hov-0002-adding-developers-object-for-development

--- constants/index.ts
+++ constants/index.ts
@@ -27,14 +27,37 @@ const NAV_BAR_ITEMS: INavItem[] = [
 
 const OUR_TEAM = [
   {
-    firstname: "",
-    lastname: "",
-    position: "",
+    firstname: "Gayane",
+    lastname: "Haruthunyan",
+    position: "Project Manager / Content Writer",
     description: "",
     image: "",
     prifile_link: "",
   },
-  {},
+  {
+    firstname: "Hovhannes",
+    lastname: "Khachatryan",
+    position: "Full-stack Developer",
+    description: "",
+    image: "",
+    prifile_link: "",
+  },
+  {
+    firstname: "Jora",
+    lastname: "Hovsepyan",
+    position: "Front-end Developer",
+    description: "",
+    image: "",
+    prifile_link: "",
+  },
+  {
+    firstname: "Rima",
+    lastname: "Alekyan",
+    position: "Front-end Developer",
+    description: "",
+    image: "",
+    prifile_link: "",
+  },
 ];
 
 export { NAV_BAR_ITEMS, BASE_URL, OUR_TEAM };
