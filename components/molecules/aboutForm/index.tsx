960833dee522e858f5c22417f7eedcbd3d2c0297
Jora <91777313+Jora001@users.noreply.github.com>
2024-08-16 23:56:46 +0400

    add cont. about

--- components/molecules/aboutForm/index.tsx
+++ components/molecules/aboutForm/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { PrimaryTextColors, SecondaryTextColors } from "@/models";
 import { Box, Flex, Heading, Text, useColorModeValue } from "@chakra-ui/react";
@@ -43,7 +44,7 @@ function AboutForm() {
         color={headerColor}
         mt={["15px", "20px", "25px", "30px", "15px"]}
       >
-        Join Lumox, learn free – just like millions already!
+        Join Lumoox, learn free – just like millions already!
       </Text>
 
       <Flex
@@ -63,13 +64,13 @@ function AboutForm() {
             mt={100}
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            ITLumox believes in YOU. Every human being is a library of stories,
-            filled with chapters of failure, victory, and valuable information.
-            It’s easy to use and helps you find people who want to hear what you
-            have to say. We strive to be a sanctuary for insightful
-            perspectives, useful knowledge, and profound wisdom. ITLumox is
-            where you can find a place to read, write, and post content that
-            matters.
+            <b>Lumoox</b> believes in YOU. Every human being is a library of
+            stories, filled with chapters of failure,victory and valuable
+            information. Our platform is easy to use and connects you with
+            people who want to hear what you have to say. We strive to be a home
+            for insightful perspectives, useful knowledge, and profound wisdom.{" "}
+            <b>Lumoox</b> is where you can find a place to <b>read, write</b>,
+            and post content that matters.
           </Text>
         </Box>
 
@@ -94,14 +95,14 @@ function AboutForm() {
             mt={8}
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            Our mission is to build a vibrant and strong online community where
-            anyone can find professional information and enhance their
+            Our <b>mission</b> is to build a vibrant and strong online community
+            where anyone can find professional information and enrich their
             knowledge. We aim to be the go-to platform for those who are eager
-            to learn and for experts who wish to share their expertise. Our
-            vision is to bridge the gap between knowledge seekers and knowledge
-            sharers, fostering a culture of continuous learning and growth.
-            Imagine a world where every single person has free access to the sum
-            of all human knowledge. That’s what we’re building at ITLumox.
+            to learn and for experts who wish to share. Our vision is to bridge
+            the gap between knowledge seekers and knowledge sharers, fostering a
+            culture of continuous learning and growth. Imagine a world where
+            every single person has free access to the sum of all human
+            knowledge. That's what we're building at <b>Lumoox</b>."
           </Text>
         </Box>
 
@@ -126,12 +127,12 @@ function AboutForm() {
             mt={8}
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            ITLumox covers a wide array of subjects, from technology and science
-            to arts and personal development. Our platform hosts articles,
-            studies, and posts on various topics, ensuring that users can easily
-            find professional and well-researched content. Our priority is
-            high-quality, accurate information that caters to both beginners and
-            advanced learners.
+            <b>Lumoox</b> covers a wide array of subjects, from technology and
+            science to arts and personal development. Our platform hosts
+            articles, studies, and posts on various topics, ensuring that users
+            can easily find <b>where to read</b> professional and
+            well-researched content. Our priority is high-quality, accurate
+            information that caters to both beginners and advanced learners.
           </Text>
         </Box>
 
@@ -156,9 +157,9 @@ function AboutForm() {
             mt={8}
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            Our community is at the heart of ITLumox. We believe that the best
+            Our community is at the heart of Lumoox. We believe that the best
             knowledge comes from collaboration and sharing diverse perspectives.
-            Our platform allows users to make a post, write articles, and
+            Our platform allows users to <b>make a post</b>, write articles, and
             participate in discussions. We encourage constructive feedback and
             open dialogue to ensure that content remains accurate and
             up-to-date. Every experience is a lesson.
@@ -174,7 +175,9 @@ function AboutForm() {
             fontSize={["20px", "22px", "24px", "26px", "22px"]}
             color={headerColor}
           >
-            FIVE STRONG PILLARS
+            FIVE STRONG PILLARS <br />
+            Our platform is built on five strong pillars that guide our
+            operations and community engagement:
           </Heading>
           <Text
             height="auto"
@@ -186,8 +189,15 @@ function AboutForm() {
             mt={8}
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            Our platform is built on five strong pillars that guide our
-            operations and community engagement:
+            1. Quality Information: Ensuring that all content is reliable,
+            well-researched, and professionally written. <br />
+            2. Accessibility: Making professional knowledge available to
+            everyone, regardless of language or background. <br /> 3. Community
+            Engagement: Encouraging active participation and collaboration among
+            users. <br /> 4. Neutrality: Maintaining a balanced and fair
+            perspective in all our content. <br /> 5. Continuous Improvement:
+            Constantly evolving and improving our platform based on user
+            feedback and technological advancements.
           </Text>
           <Box
             as="ol"
@@ -195,28 +205,7 @@ function AboutForm() {
             textAlign="left"
             lineHeight="2.5"
             color={headerColor}
-          >
-            <Box as="li">
-              Quality Information: Ensuring that all content is reliable,
-              well-researched, and professionally written.
-            </Box>
-            <Box as="li">
-              Accessibility: Making professional knowledge available to
-              everyone, regardless of language or background.
-            </Box>
-            <Box as="li">
-              Community Engagement: Encouraging active participation and
-              collaboration among users.
-            </Box>
-            <Box as="li">
-              Neutrality: Maintaining a balanced and unbiased perspective in all
-              our content.
-            </Box>
-            <Box as="li">
-              Continuous Improvement: Constantly evolving and improving our
-              platform based on user feedback and technological advancements.
-            </Box>
-          </Box>
+          ></Box>
         </Box>
 
         <Box mt="50px" px={[4, 6, 8, 10]}>
@@ -240,9 +229,17 @@ function AboutForm() {
             mt={8}
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            Quality is a cornerstone of ITLumox. We employ rigorous quality
-            control measures to ensure that all content is trustworthy and
-            valuable:
+            Quality is a cornerstone of <b>Lumoox</b>. We employ quality control{" "}
+            <br />
+            measures to ensure that all content is trustworthy and valuable:{" "}
+            <br />- <b>Verification</b>: All submissions are reviewed for
+            accuracy and supported by credible sources. <br />-{" "}
+            <b> Editorial Oversight</b> : Our editorial team ensures that
+            articles <br /> adhere to our standards of quality and relevance.{" "}
+            <br />- <b>User Reviews</b>: Community members can rate and review
+            articles, providing additional layers of quality assurance. <br />-
+            <b>Anti-Vandalism</b>: We have robust systems in place to detect and
+            prevent vandalism or malicious edits.
           </Text>
           <Box
             as="ul"
@@ -250,25 +247,7 @@ function AboutForm() {
             textAlign="left"
             lineHeight="2"
             color={headerColor}
-          >
-            <Box as="li">
-              Verification: All submissions are reviewed for accuracy and
-              supported by credible sources.
-            </Box>
-            <Box as="li">
-              Expert Review: Experienced professionals in various fields review
-              content for technical accuracy and relevance.
-            </Box>
-            <Box as="li">
-              Continuous Updates: Our team regularly updates content to reflect
-              new information and advancements in the field.
-            </Box>
-            <Box as="li">
-              Community Feedback: We actively seek feedback from our community
-              to identify areas for improvement and ensure content remains
-              relevant.
-            </Box>
-          </Box>
+          ></Box>
         </Box>
 
         <Box mt="50px" px={[4, 6, 8, 10]}>
@@ -280,7 +259,7 @@ function AboutForm() {
             fontSize={["20px", "22px", "24px", "26px", "22px"]}
             color={headerColor}
           >
-            USER EXPERIENCE
+            GLOBAL REACH
           </Heading>
           <Text
             height="auto"
@@ -292,67 +271,17 @@ function AboutForm() {
             mt={8}
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            User experience is at the core of our design philosophy. Our
-            platform is user-friendly, intuitive, and accessible. We offer a
-            seamless browsing experience, making it easy for users to find the
-            information they need. We continuously gather feedback and make
-            improvements to enhance the overall user experience.
-          </Text>
-        </Box>
-
-        <Box mt="50px" px={[4, 6, 8, 10]}>
-          <Heading
-            height="auto"
-            width="100%"
-            mb="10px"
-            textAlign="left"
-            fontSize={["20px", "22px", "24px", "26px", "22px"]}
-            color={headerColor}
-          >
-            DATA SECURITY
-          </Heading>
-          <Text
-            height="auto"
-            width="100%"
-            textAlign="left"
-            lineHeight="2"
-            color={primaryTextColor}
-            fontFamily="Inter, sans-serif"
-            mt={8}
-            fontSize={["16px", "18px", "20px", "22px", "20px"]}
-          >
-            At ITLumox, we take data security seriously. We implement robust
-            security measures to protect user data and ensure privacy. Our
-            platform complies with industry standards and regulations to
-            safeguard personal information and maintain user trust.
-          </Text>
-        </Box>
-
-        <Box mt="50px" px={[4, 6, 8, 10]} mb="50px">
-          <Heading
-            height="auto"
-            width="100%"
-            mb="10px"
-            textAlign="left"
-            fontSize={["20px", "22px", "24px", "26px", "22px"]}
-            color={headerColor}
-          >
-            JOIN US
-          </Heading>
-          <Text
-            height="auto"
-            width="100%"
-            textAlign="left"
-            lineHeight="2"
-            color={primaryTextColor}
-            fontFamily="Inter, sans-serif"
-            mt={8}
-            fontSize={["16px", "18px", "20px", "22px", "20px"]}
-          >
-            Join the ITLumox community today and become a part of a dynamic and
-            vibrant network of knowledge seekers and sharers. Together, we can
-            build a better future by spreading knowledge and fostering learning
-            across the globe.
+            While Lumoox prioritizes Armenian content, we are committed to being
+            a global platform. We offer articles and resources in English to
+            serve a diverse audience. Our language editions are managed by
+            dedicated editorial teams to ensure quality and cultural relevance.{" "}
+            <br />
+            Thus ,We're not chasing clicks or selling your data. We're fueled by
+            a community hungry for knowledge and eager to share. We believe in
+            the power of community-driven enlightenment. The idea that you can
+            be a teacher to someone will push you to be the person you dreamed
+            of when you needed a teacher. Welcome to Lumoox – where curiosity
+            finds its home and learning is luminously free!"
           </Text>
         </Box>
       </Flex>
