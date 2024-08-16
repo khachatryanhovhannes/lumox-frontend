c98aaa6d05939f75049d0cd8a4a60e272e6e39e1
Jora <91777313+Jora001@users.noreply.github.com>
2024-08-16 17:20:28 +0400

    added content

--- components/molecules/helpForm/index.tsx
+++ components/molecules/helpForm/index.tsx
@@ -36,7 +36,7 @@ function HelpForm() {
         mt={["20px", "25px", "30px", "35px", "40px"]}
         lineHeight={["1", "0.9", "0.8", "0.7", "1.3"]}
       >
-        How can we help?{" "}
+        HELP
       </Heading>
 
       <Text
@@ -47,7 +47,7 @@ function HelpForm() {
         color={secondaryTextColor}
         mt={["15px", "20px", "25px", "30px", "15px"]}
       >
-        Join the millions learning to code with Lumox for free{" "}
+        Welcome to Lumoox Help Center!{" "}
       </Text>
       <Box mt="40px">
         <Search />
@@ -69,12 +69,11 @@ function HelpForm() {
             fontFamily="Inter, sans-serif"
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            Your Gateway to Knowledge and <b>Support!</b> Whether youre here to
-            read insightful <b>articles</b>, write and <b>post</b> your own
-            experiences, or simply expand your knowledge, this guide will help
-            you use our platform with ease. Our mission is to provide
-            accessible, professional <b>information</b> to everyone. How can you
-            make the most of ITLumox?
+            Your Gateway to Knowledge and Support! Whether you’re here to read
+            insightful articles, write and post your own experiences, or simply
+            expand your knowledge, this guide will help you to use our platform
+            with ease. Our mission is to provide accessible, professional
+            information to everyone.
           </Text>
         </Box>
 
@@ -98,20 +97,20 @@ function HelpForm() {
             fontFamily="Inter, sans-serif"
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            <b>ITLumox</b> is designed to be user-friendly and intuitive. Heres
+            <b>Lumoox</b> is designed to be user-friendly and intuitive. Here’s
             how you can get started:
             <br />
-            <b>1. Sign Up:</b> Creating an account on <b>ITLumox</b> is simple.
-            Click on the <b>Sign Up</b> button at the top right corner of the
-            homepage, fill in your details, and youre ready to go.
+            <b>1. Sign Up:</b> Creating an account on <b>Lumoox</b> Click on the
+            Sign Up button at the top right corner of the <b>homepage</b>, fill
+            in your details, and you’re ready to go.
             <br />
             <b>2. Profile Setup:</b> Once signed up, complete your profile. Add
             a photo, a brief bio, and any other relevant information. This helps
             others know more about you and your interests.
             <br />
-            <b>3. Explore Content:</b> Use the search bar to find articles on
-            topics that interest you. You can search for professional
-            information, learning resources, and more.
+            <b>3. Explore Content:</b> Use the <b>search bar</b> to{" "}
+            <b>find articles</b> on topics that interest you. You can search for
+            <b>professional information, learning resources,</b> and more.
           </Text>
         </Box>
 
@@ -135,23 +134,24 @@ function HelpForm() {
             fontFamily="Inter, sans-serif"
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
-            Creating content on <b>ITLumox</b> is straightforward. Follow these
-            steps to write and post your articles:
+            Creating content on <b>Lumoox</b> is straightforward. Follow these
+            steps to <b>write</b> and post your <b>articles</b>.
             <br />
             <b>1. Start Writing:</b> Click on the <b>Write</b> button on the
-            homepage. This will take you to the article editor.
+            homepage. This will take you to the <b>article editor.</b>
             <br />
-            <b>2. Compose Your Article:</b> Use the editor to write your
-            article. You can format your text, add images, and include links to
-            make your article engaging.
+            <b>2. Compose Your Article:</b> Use the <b>editor</b> to{" "}
+            <b>write</b> your <b>article</b>. You can format your text, add
+            images, and include links to make your <b>article</b> engaging.
             <br />
-            <b>3. Add Keywords:</b> Keywords help your article get discovered.
-            Include relevant terms such as information, learning, study, where
-            to read, and others that match your content.
+            <b>3. Add Keywords:</b> Keywords help your <b>article</b> get
+            discovered. Include relevant terms such asb
+            <b> information, learning, study</b>, and others that match your
+            content.
             <br />
             <b>4. Review and Publish:</b> Once youre satisfied with your
-            article, click the <b>Publish</b> button. Your article will be live
-            for everyone to read.
+            article, click the <b>Publish</b> button. Your <b>article</b> will
+            be live for everyone to read.
           </Text>
         </Box>
 
@@ -176,19 +176,19 @@ function HelpForm() {
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
             {" "}
-            <b>ITLumox</b> offers a vast library of articles. Heres how to find
-            what youre looking for:
+            Here’s how to find what you’re looking for:
             <br />
-            <b>1. Search Function:</b> Use the search bar at the top of the
-            page. Enter keywords or phrases related to your topic of interest.
+            <b>1. Search Function:</b> Use the <b>search bar</b> at the top of
+            the page. Enter keywords or phrases related to your topic of
+            interest.
             <br />
-            <b>2. Categories:</b> Browse through different categories to
-            discover articles. Categories include Technology, Science, Arts,
-            Business, and more.
+            <b>2. Categories:</b>Categories: Browse through different categories
+            to discover <b>articles</b>. Categories include{" "}
+            <b>Technology, Science, Arts, Business</b>, and more.
             <br />
-            <b>3. Recommendations:</b> Based on your reading habits,{" "}
-            <b>ITLumox</b> will suggest articles that might interest you. Check
-            out the recommendations section on your homepage.
+            <b>3. Recommendations:</b> : Based on your reading habits, Lumoox
+            will suggest articles that might interest you. Check out the <br />
+            <b>recommendations</b> section on your <b>homepage</b>.
           </Text>
         </Box>
 
@@ -213,17 +213,15 @@ function HelpForm() {
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
             {" "}
-            Engaging with content on <b>ITLumox</b> is easy and rewarding:
+            <b>1. Commenting:</b> Share your thoughts on <b>articles</b> by
+            leaving comments. This fosters discussion and allows you to connect
+            with other readers and writers.
             <br />
-            <b>1. Commenting:</b> Share your thoughts on articles by leaving
-            comments. This fosters discussion and allows you to connect with
-            other readers and writers.
-            <br />
-            <b>2. Sharing:</b> Found an article you love? Share it on social
-            media or with friends using the share button.
+            <b>2. Sharing:</b> Found an <b>article</b> you love? <b>Share</b> it
+            on social media or with friends using the <b>share</b> button.
             <br />
             <b>3. Liking:</b> Show your appreciation for good content by
-            clicking the Like button on articles.
+            clicking the <b>Like</b> button on <b>articles</b>.
           </Text>
         </Box>
 
@@ -249,16 +247,17 @@ function HelpForm() {
           >
             {" "}
             As a writer, building a following is key to your success on{" "}
-            <b>ITLumox</b>. Heres how to do it:
+            <b>Lumoox</b>. Heres how to do it:
             <br />
-            <b>1. Consistency:</b> Post regularly to keep your readers engaged.
+            <b>1. Consistency:</b> <b>Post</b> regularly to keep your readers
+            engaged.
             <br />
             <b>2. Quality Content:</b> Focus on creating high-quality,
-            informative articles. The better your content, the more likely you
-            are to attract readers.
+            <b> informative articles</b>. The better your content, the more
+            likely you are to attract readers.
             <br />
-            <b>3. Engage:</b> Respond to comments on your articles and engage
-            with your readers. This builds a loyal audience.
+            <b>3. Engage:</b> Respond to comments on your <b>articles</b> and
+            engage with your readers. This builds a loyal audience.
           </Text>
         </Box>
 
@@ -283,15 +282,17 @@ function HelpForm() {
             fontSize={["16px", "18px", "20px", "22px", "20px"]}
           >
             {" "}
-            <b>ITLumox</b> offers several features to enhance your experience:
+            <b>Lumoox</b> offers several features to enhance your experience:
             <br />
-            <b>1. Drafts:</b> Save your articles as drafts and come back to them
-            later.
+            <b>1. Drafts:</b> Save your work-in-progress <b>articles</b> as{" "}
+            <b>drafts</b>. You can return to them anytime to continue{" "}
+            <b>writing</b>.
             <br />
-            <b>2. Notifications:</b> Stay updated with notifications for
-            comments, likes, and new articles from your favorite writers.
+            <b>2. Bookmarks:</b> Bookmark <b>articles</b> to read later. You can
+            access your bookmarks from <b>your profile</b>.
             <br />
-            <b>3. Bookmarking:</b> Bookmark articles to read later.
+            <b>3. Analytics:</b>View analytics on your <b>articles</b> to see
+            how many people have <b>read , liked</b> , and commented on them.
           </Text>
         </Box>
 
@@ -306,16 +307,28 @@ function HelpForm() {
           >
             Frequently Asked Questions (FAQs){" "}
           </Heading>
-          <Text textAlign="left" lineHeight="2" color={primaryTextColor}>
-            1. How do I create an account? <br />
-            To create an account, click on the Sign Up button at the top right
-            corner and follow the instructions. 2. Can I edit my articles after
-            publishing? <br />
-            Yes, you can edit your articles even after publishing. Go to your
-            profile, find the article, and click Edit. <br /> 3. How do I report
-            inappropriate content? <br />
-            If you come across any inappropriate content, click on the Report
-            button located on the article or comment.
+          <Text
+            textAlign="left"
+            lineHeight="2"
+            color={primaryTextColor}
+            fontSize={["16px", "18px", "20px", "22px", "20px"]}
+          >
+            1. <b>How do I create an account?</b> <br />
+            To create an account, click on the <b>Sign Up</b> button at the top
+            right corner and follow the <b>instructions</b>. <br />
+            2.{" "}
+            <b>
+              Can I edit my <b>articles</b> after publishing?
+            </b>{" "}
+            <br />
+            Yes, you can edit your <b>articles</b> even after publishing. Go to
+            <b> your profile</b>, find the <b>article</b>, and click <b>Edit</b>
+            <br />
+            <b>3. How do I report inappropriate content?</b>. <br />
+            If you come across any inappropriate content, click on the{" "}
+            <b>Report</b>
+            <br />
+            button located on the <b>article</b> or comment.
           </Text>
         </Box>
         <Box mt="70px" width="80%" mx="auto">
@@ -329,15 +342,22 @@ function HelpForm() {
           >
             Contact Us{" "}
           </Heading>
-          <Text textAlign="left" lineHeight="2" color={primaryTextColor}>
+          <Text
+            textAlign="left"
+            lineHeight="2"
+            color={primaryTextColor}
+            fontSize={["16px", "18px", "20px", "22px", "20px"]}
+          >
             If you have any questions or need further assistance, feel free to
-            contact our support team. You can reach us via email at
-            support@itlumox.com or <br /> Community Guidelines <br />
+            contact our <b>support</b> team. You can reach us <b>via email</b>{" "}
+            at <br />
+            <b>support@lumoox.coms</b> or through the contact form on our
+            website. <br /> Community Guidelines <br />
             To ensure a positive and respectful environment, please adhere to
-            our community guidelines: <br />
-            - Respect others’ opinions and perspectives. <br /> - Avoid using
-            offensive language or engaging in personal attacks. <br />- Provide
-            constructive feedback and engage in healthy discussions.
+            our <b>community guidelines: </b>
+            <br />- Respect others’ opinions and perspectives. <br />- Avoid
+            using offensive language or engaging in personal attacks. <br />-
+            Provide constructive feedback and engage in healthy discussions.
           </Text>
         </Box>
         <Box mt="70px" width="80%" mx="auto">
@@ -351,16 +371,22 @@ function HelpForm() {
           >
             Best Practices for Writers{" "}
           </Heading>
-          <Text textAlign="left" lineHeight="2" color={primaryTextColor}>
-            To make your articles stand out, consider these best practices:{" "}
-            <br /> 1. Original Content: Ensure your articles are original and
-            not copied from other sources. This builds credibility and trust
-            with your readers. <br />
+          <Text
+            textAlign="left"
+            lineHeight="2"
+            color={primaryTextColor}
+            fontSize={["16px", "18px", "20px", "22px", "20px"]}
+          >
+            To make your <b>articles</b> stand out, consider these best
+            practices: <br /> 1. Original Content: Ensure your <b>articles</b>{" "}
+            are original and not copied from other sources. This builds
+            credibility and trust with your readers. <br />
             2. Engaging Titles: Create compelling and clear titles that attract
             readers’ attention. <br />
             3. Clear Structure: Use headings, subheadings, and bullet points to
-            make your articles easy to read. <br /> 4.Visuals: Include relevant
-            images, graphs, or videos to enhance your articles. <br />
+            make your <b>articles</b> easy to read. <br /> 4.Visuals: Include
+            relevant images, graphs, or videos to enhance your <b>articles</b>.{" "}
+            <br />
             5. References: Cite your sources when necessary to add authenticity
             to your work.
           </Text>
@@ -376,16 +402,18 @@ function HelpForm() {
           >
             Benefits of Using ITLumox{" "}
           </Heading>
-          <Text textAlign="left" lineHeight="2" color={primaryTextColor}>
-            ITLumox is not just another platform; it’s a community of knowledge
-            seekers and sharers. Here are some benefits: <br /> 1.
-            Accessibility: Access a wide range of professional information.{" "}
-            <br />
-            2. Community: Connect with like-minded individuals and experts.{" "}
-            <br />
+          <Text
+            textAlign="left"
+            lineHeight="2"
+            color={primaryTextColor}
+            fontSize={["16px", "18px", "20px", "22px", "20px"]}
+          >
+            1.Accessibility: Access a wide range of professional{" "}
+            <b>information</b>. <br /> 2. Community: Connect with like-minded
+            individuals and experts. <br />
             3. Growth: Enhance your learning and expand your knowledge base.{" "}
             <br />
-            4. Impact: Share your insights and experiences to help others.
+            4. Impact: Share your insights and experiences to help others.{" "}
           </Text>
         </Box>
       </Flex>
